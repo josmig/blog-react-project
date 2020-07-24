d01111e5d22c3991428ee42ffd3e926f73035b78
josmig <pantigosomiguel59@gmail.com>
2020-07-24 18:37:31 -0500

    estructura de proyecto back-webstie

---
+++ back-website/models/user.js
@@ -0,0 +1,11 @@
+//construccion del modelo user
+const user = {
+    name: String,
+    lastname: String,
+    email: {
+        type: String,
+        unique: true
+    },
+    password: String,
+    active: Boolean
+};
