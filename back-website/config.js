86336a4cb0383536703e34d1d2e84530121d3cd0
josmig <pantigosomiguel59@gmail.com>
2020-08-31 20:07:45 -0500

    conexión mongodb + appserver  , agregando cambios automaticamente con nodemon

--- back-website/config.js
+++ back-website/config.js
@@ -1,7 +1,9 @@
 const API_VERSION ="v1";
 const IP_SERVER = "localhost";
+const PORT_DB =27017;
 
 module.exports = {
     API_VERSION,
-    IP_SERVER
+    IP_SERVER,
+    PORT_DB
 }
--- back-website/index.js
+++ back-website/index.js
@@ -2,8 +2,22 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 const port = process.env.PORT || 3977;
-const port_db = 27017;
-const { API_VERSION, IP_SERVER } = require('./config');
+const { API_VERSION, IP_SERVER , PORT_DB} = require('./config');
 
+mongoose.set("useFindAndModify", false);
 
-mongoose.connect(`mongodb://${IP_SERVER}:${port_db}`)
+mongoose.connect(`mongodb://${IP_SERVER}:${PORT_DB}/bd_myfirtsweb`,
+    {useNewUrlParser: true , useUnifiedTopology: true}, 
+    (err,res) => {
+        if(err){
+            throw err;    
+        }else{
+            console.log("La conexión a la BD es correcta!");
+            app.listen(port, ()=>{
+                console.log("##############");
+                console.log("####API REST####");
+                console.log("##############");
+                console.log(`http://${IP_SERVER}:${port}/api/${API_VERSION}/`);
+            })
+        }
+});
