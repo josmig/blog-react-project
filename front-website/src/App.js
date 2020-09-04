e51321e6f8fbfc20682bafb93ba52382c031be8b
josmig <pantigosomiguel59@gmail.com>
2020-09-03 23:53:07 -0500

    manejo de react-router-dom

--- front-website/src/App.js
+++ front-website/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 /* import './App.css'; */
-import { DatePicker,Card } from 'antd';
 import './scss/index.scss';
-
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 function App() {
 
-  let nombre = 'Miguel Pantigoso'
-
   return (
-    
-    <div>
-      <h1 class="App">Funciones</h1>
-      <DatePicker/>
-      <Card
-        title="Default"
-        extra={<a href="#">Más..</a>}
-        style={{width:380}}>
-        <p>{nombre}</p>
-      </Card>
+    <Router>
+    <div className="app"> 
+      <h1>Sistema de rutas basicas</h1>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/contact" component={Contact}/>
     </div>
+    </Router> 
   );
 }
-
+//componentes
+function Home(){
+  return <h2>Estamos en el componente home..</h2>
+}
+function Contact(){
+  return <h2>Componente contacto!!</h2>
+}
+function User(){
+  return <h2>Estamos en el componente Users</h2>
+}
 export default App;
