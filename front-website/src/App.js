7c18fcc5aca79bb9f06a7b645a533402a422fced
josmig <pantigosomiguel59@gmail.com>
2020-09-27 21:02:23 -0500

    sistema de rutas para tirar el layout correspondiente

--- front-website/src/App.js
+++ front-website/src/App.js
@@ -2,16 +2,31 @@ import React from 'react';
 import './scss/index.scss';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import routes from './config/routes'
-import AdminHome from './pages/Admin'
+
 function App() {
 
   return (
     <Router>
       <Switch>
-        <Route path="/admin" exact="true" component={AdminHome}/>
+        {routes.map((route,index) => (
+          <RouteWithSubRoutes key={index} {...route} />
+        ))}
       </Switch>
     </Router>
   );
 }
+//hook comienza con use
+//Componenet comienza con Mayuscula
+//funcion normal en minisucla
+function RouteWithSubRoutes(route){
+  console.log(route);
+  return (
+    <Route 
+      path={route.path}
+      exact={route.exact}
+      render={props => <route.component routes={route.routes}{...props}/>}        
+    />
+  );
+};
 
 export default App;
