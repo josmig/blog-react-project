1e04a4a5132a5166fdfcef4a485399eae3e8d8b7
josmig <pantigosomiguel59@gmail.com>
2020-09-28 18:13:29 -0500

    sistema de rutas dentro de layoutAdmin

--- front-website/src/layouts/LayoutAdmin.js
+++ front-website/src/layouts/LayoutAdmin.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import {Layout} from "antd";
+import { Route } from 'react-router-dom';
+import "./LayoutAdmin.scss";
 
-export default function LayoutAdmin(){
+export default function LayoutAdmin(props){
+    /* console.log(props); */
+    //haciendo destructurin de Layout trayendo los demas components del layout
+    const {Header,Content,Footer} = Layout; 
+    //destructuracion trayendo solamente los routes de los props
+    const { routes}= props
+
     return(
         <Layout>
             <h2>Menu Sider</h2>
-            <div>
-                Cotenido...
-            </div>
-            <h5>Footer</h5>
+            <Layout>
+                <Header>Cabecera</Header>
+                <Content>
+                    <LoadRouter routes={routes}/>
+                </Content>
+                <Footer>
+                    José Miguel Pantigoso &copy; 2020
+                </Footer>
+            </Layout>
         </Layout>        
     );
 }
+
+function LoadRouter({routes}){
+    
+    return routes.map((route,index) => (
+        <Route            
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.component}
+            
+        />        
+    ));
+}
+
+
+
